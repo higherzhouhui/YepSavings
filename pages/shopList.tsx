df5961cc9b6b6babd3c11524156e108485039862
higherzhouhui <782492184@qq.com>
2023-03-23 22:55:46 +0800

    wanshan

--- pages/shopList.tsx
+++ pages/shopList.tsx
@@ -44,6 +44,9 @@ const ShopList: NextPage = () => {
     },
   ])
   const handleSearch = () => {
+    if (!searchValue) {
+      setProductList([])
+    }
   }
   const onChangeVluae = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value
@@ -58,14 +61,16 @@ const ShopList: NextPage = () => {
     setProductList([...productList])
   }
   return (
-    <div className={`${style.ShopListContainer} container mx-auto`}>
+    <div className={`${style.ShopListContainer} container mx-auto relative`}>
       <Navbar title='My Shopping List' />
        <div className={`${style.content}`}>
         <div className={style.addItemInputWrapper}>
           <input placeholder="+ Add an item" value={searchValue} onChange={(e) => {onChangeVluae(e)}} onKeyDown={(e) => onkeydown(e)} />
           <div className={style.confirmBtn} onClick={handleSearch}>Done</div>
         </div>
-        <div className={style.pageList}>
+        {
+          productList.length ? <>
+            <div className={style.pageList}>
           {
             productList.map((item, index) => {
               return <ShopItem {...item} key={index} handleClick={() => handleClick(index)}/>
@@ -76,7 +81,16 @@ const ShopList: NextPage = () => {
           <div className='rounded-full mr-[12px] h-[22px] w-[22px] border border-[rgb(209,209,209)]'></div>
           <div className='text-[#2B2C43]'>MILK</div>
         </div>
-        <div className='w-full mt-8 flex items-center rounded-md shadow-md justify-center h-12 text-[#fff] bg-[#1A77F2] text-lg'>Delete All</div>
+        <div className='shadow flex w-full mt-4 h-12 bg-[white] items-center px-4 rounded'>
+          <div className='rounded-full mr-[12px] h-[22px] w-[22px] border border-[rgb(209,209,209)]'></div>
+          <div className='text-[#2B2C43]'>FOOD</div>
+        </div>
+        <div className='w-full mt-8 flex items-center rounded-md shadow-md justify-center h-12 text-[#fff] bg-[#1A77F2] text-lg'>
+          Delete All
+        </div></> : <div className='flex items-center justify-center min-h-full absolute w-full'>
+          <div><img src='/static/image/empty.png' alt='empty' className='w-36 h-36' /></div>
+        </div>
+        }
        </div>
     </div>
   )
